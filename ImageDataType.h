b060d3b362d358569a52c3c7ce97ea61d7f4dd75
Rink37 <95770789+Rink37@users.noreply.github.com>
2025-05-08 21:56:46 +0100

    Add files via upload

--- ImageDataType.h
+++ ImageDataType.h
@@ -2,10 +2,17 @@
 #define IMAGEDATA
 
 struct imageData{
-    const int Width;
-    const int Height;
-    const int Channels;
-    const unsigned char *Bytes;
+    imageData(const int w, const int h, const int c, unsigned char* b) {
+        Width = w;
+        Height = h;
+        Channels = c;
+        Bytes = b;
+    }
+
+    int Width;
+    int Height;
+    int Channels;
+    unsigned char* Bytes;
 };
 
 #endif
