b5adbb6cc3caa90f79b6c8e4240fe4a165a372a5
Rink37 <95770789+Rink37@users.noreply.github.com>
2025-06-15 20:58:41 +0100

    Created new stand-in UI elements

--- textures/ImageDataType.h
+++ textures/ImageDataType.h
@@ -2,17 +2,10 @@
 #define IMAGEDATA
 
 struct imageData{
-    imageData(const int w, const int h, const int c, unsigned char* b) {
-        Width = w;
-        Height = h;
-        Channels = c;
-        Bytes = b;
-    }
-
-    int Width;
-    int Height;
-    int Channels;
-    unsigned char* Bytes;
+    const int Width;
+    const int Height;
+    const int Channels;
+    const unsigned char *Bytes;
 };
 
 #endif
