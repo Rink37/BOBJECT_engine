43d09861747268252c0f5ed7a159ef7f4e372f4e
Rink37 <robdanshaw@Gmail.com>
2026-04-29 10:51:25 +0100

    Font advances can now be stored in font data

--- fonts/ConvertFont.cpp
+++ fonts/ConvertFont.cpp
@@ -17,11 +17,15 @@ void loadAndWriteFile(string path, int colCount, int rowCount, int cellWidth, in
     string name = path.substr(path.find_last_of("/\\")+1);
     string::size_type const p(name.find_last_of('.'));
     string nameNoExt = name.substr(0, p);
+    string binName = nameNoExt + string("_advances.bin");
     string capNameNoExt = nameNoExt;
     std::transform(capNameNoExt.begin(), capNameNoExt.end(), capNameNoExt.begin(), ::toupper);
     unsigned char* pixels = stbi_load(path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+    ifstream advances;
+    advances.open(location+string("\\")+binName, ios::binary | ios::in); 
     string outname = location+string("\\headers\\")+nameNoExt+string(".h");
     cout << outname << endl;
+    cout << location + string("\\") + binName << endl;
     ofstream out(outname.c_str());
     out << string("#ifndef ")+capNameNoExt+string("BYTES\n");
     out << string("#define ")+capNameNoExt+string("BYTES\n");
@@ -36,8 +40,20 @@ void loadAndWriteFile(string path, int colCount, int rowCount, int cellWidth, in
     out << string("const int ")+nameNoExt+string("Channels = ");
     out << texChannels;
     out << ";\n\n";
+    out << string("const float")+nameNoExt+string("Advances[] = {");
+    for (int i = 0; i != 94; i++){
+        float advance;
+        advances.read((char*)(&advance), sizeof(float));
+        cout << advance << endl;
+        if (i < 93){
+            out << to_string(advance) + string("f, ");
+        } else {
+            out << to_string(advance);
+        }
+    }
+    out << "};\n\n";
     out << string("unsigned char ")+nameNoExt+string("Bytes[] = { ");
-    cout << texChannels << endl;
+    //cout << texChannels << endl;
     size_t imagesize = texWidth*texHeight*4;
     for (size_t i = 0; i!= imagesize; i += 5-texChannels){
         out << "0x" << hex << setw(2) << setfill('0') << (int)(unsigned char) pixels[i];
--- fonts/ConvertFontLayout.py
+++ fonts/ConvertFontLayout.py
@@ -1,5 +1,6 @@
 import json
 import os
+from array import array
 
 filename = "coolvetica_sdf_layout.json"
 
@@ -7,8 +8,18 @@ if (os.path.exists(filename)):
     with open(filename) as f:
         data = json.load(f);
         f.close()
-    
-print(data["glyphs"])
 
-for glyph in data["glyphs"]:
-    print(glyph["unicode"])
+glyphs = data["glyphs"][1:]
+
+advances = []
+
+for glyph in glyphs:
+    bounds = glyph["planeBounds"]
+    #print(glyph["unicode"], glyph["advance"], bounds["right"] - bounds["left"])
+    advances += [glyph["advance"]]
+
+print(len(advances))
+output_file = open("coolvetica_sdf_advances.bin", 'wb')
+advances_array = array('f', advances)
+advances_array.tofile(output_file)
+output_file.close()
